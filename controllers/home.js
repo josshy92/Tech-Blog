f622c967ddcb007c85864645381a06d10a319755
Josshy <josshy92@hotmail.com>
2022-01-09 14:45:07 -0600

    added homepage handlebar, seeds, auth and helpers

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
-//need to require model for blogs
+// require model for blogs and users
+const { Blog, User } = require("../models");
+const withAuth = require("../utils/auth")
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        const blogs = blogData.map((blog) => blog.get({plain: true}));
+
+        res.render("homepage", {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
 
